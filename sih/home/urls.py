533a3ab6f0935e5a3be223d77fdf99eb5ae19b94
Abhiram B.S.N <abhirambsn@gmail.com>
2022-04-08 22:14:58 +0530

    Basic Implementation of Audio to Text Done

--- sih/home/urls.py
+++ sih/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('transcribe', views.transcribe, name='transcribe')
 ]
--- sih/home/views.py
+++ sih/home/views.py
@@ -1,4 +1,38 @@
+from random import sample
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+
+from google.cloud import speech
+
+from dotenv import load_dotenv
+
+load_dotenv("../../")
+
+def transcribe(request):
+    if (request.method != "POST"):
+        return render(request, "home/transcribe.html")
+    else:
+        client = speech.SpeechClient()
+        file = request.FILES['file']
+        if (file):
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+            file_url = fs.url(filename)
+            audioFile = None
+            with fs.open("D:\\FullStackDevelopment\\sih_aiml\\sih"+file_url, 'rb') as audioFileObj:
+                audioFile = audioFileObj.read()
+            audio = speech.RecognitionAudio(content=audioFile)
+            config = speech.RecognitionConfig(
+                enable_automatic_punctuation=True,
+                sample_rate_hertz=48000,
+                language_code="en-US"
+            )
+
+            response = client.recognize(config=config, audio=audio)
+            spresults = ""
+            for result in response.results:
+                spresults+=result.alternatives[0].transcript
+        return render(request, "home/success.html", {"result": spresults})
 
 def index(request):
     return render(request, "home/index.html")
