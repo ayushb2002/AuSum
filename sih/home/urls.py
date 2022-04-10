07db3eb9f2091fe33e97a0ec890da3e09f2020e6
AlcatrazFP <omi798682@gmail.com>
2022-04-10 10:12:40 +0530

    new auth

--- sih/home/urls.py
+++ sih/home/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('transcribe', views.transcribe, name='transcribe')
+    path('transcribe', views.transcribe, name='transcribe'),
+    path('register/', views.registerPage,name="register"),
+    path('login/', views.loginPage,name="login"),
+    path('logout/', views.loginPage,name="logout"),
+
 ]
--- sih/home/views.py
+++ sih/home/views.py
@@ -1,14 +1,53 @@
+from multiprocessing import context
 from random import sample
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.core.files.storage import FileSystemStorage
-
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 from google.cloud import speech
-
 from dotenv import load_dotenv
-
-from .forms import UploadForm
+from .forms import UploadForm, CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
 
 load_dotenv("../../")
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method=='POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for '+user)
+            return redirect('login')
+
+    context={'form':form}
+    return render(request,"home/register.html", context)
+
+def loginPage(request):
+
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user = authenticate(request, username=username,password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+
+
+    context={}
+    return render(request,"home/login.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def transcribe(request):
     if (request.method != "POST"):
